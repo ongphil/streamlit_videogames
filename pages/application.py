4e30db2f8e1cb16e3c36dff4d12c90ce3f15f5c8
Philippe Ong <ong.philippe@gmail.com>
2024-03-23 22:40:36 +0100

    remove plotly express

--- pages/application.py
+++ pages/application.py
@@ -2,7 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import time
-import plotly.express as px
 import matplotlib.pyplot as plt
 import seaborn as sns
 from os import path
--- presentation.py
+++ presentation.py
@@ -1,8 +1,5 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
-import time
-import plotly.express as px
 
 st.set_page_config(
     page_title="Presentation",
